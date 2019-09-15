27751686bb9ffd2e3fe8dbe27a316d90a7db637a
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-15 19:07:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  update();
+});
+ 
+
+
+function update(){
+let par= document.getElementsByTagName('p')[0];
+
+par.textContent ="This is really cool!";
+}
+
